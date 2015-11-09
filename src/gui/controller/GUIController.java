e760d10b839069b55b19f01713c1a2f2fddcaed7
Sam Montoya <smon7958@740s-pl-118.local>
2015-11-09 12:18:45 -0700

    Added to Repo

---
+++ src/gui/controller/GUIController.java
@@ -0,0 +1,23 @@
+package gui.controller;
+
+import gui.view.GUIFrame;
+
+/**
+ * Controller for the simple GUI.
+ * @author Sam Montoya
+ * @version 1.0, Nov 9
+ */
+public class GUIController
+{
+	/**
+	 * Reference to the GUIFrame obect of the view.
+	 */
+	private GUIFrame baseFrame;
+	
+	public GUIController()
+	{
+		baseFrame = new GUIFrame(this);
+	}
+	public void start()
+	{}
+}
---
+++ src/gui/controller/GUIRunner.java
@@ -0,0 +1,17 @@
+package gui.controller;
+
+/**
+ * Launches the GUI Program
+ * @author Sam Montoya
+ * @version 1.0 Nov 9, 2015
+ * 
+ */
+
+public class GUIRunner
+{
+	public static void main(String[]args)
+	{
+	GUIController startController = new GUIController();
+	startController.start();
+	}
+}
